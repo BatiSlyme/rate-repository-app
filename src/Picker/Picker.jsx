74a9c76b99fafa3850b1a498487adc23eb392d9b
SBakalov <SBakalov@technologica.com>
2025-03-27 17:11:20 +0200

    adding a headerComponent and a Modal with Menu items inside //todo implement filtering via gql

---
+++ src/Picker/Picker.jsx
@@ -0,0 +1,63 @@
+// import * as React from 'react';
+import { View, StyleSheet } from 'react-native';
+import { Menu } from 'react-native-paper';
+import Text from '../Text';
+
+const styles = StyleSheet.create({
+    modalOverlay: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'rgba(0, 0, 0, 0.5)', // Optional: semi-transparent backdrop
+        zIndex: 1000, // Ensure it's above everything
+    },
+    modalContent: {
+        width: '80%',
+    },
+    innerContainer: {
+        backgroundColor: 'white',
+        padding: 20,
+        borderRadius: 8,
+    },
+    menuContainer: {
+        paddingTop: 20,
+    },
+});
+
+
+import { Modal as RNModal } from 'react-native';
+
+const MyComponent = ({ visible, setVisible, setTitle }) => {
+    const onTitleChange = (title) => {
+        setTitle(title);
+        setVisible(!visible);
+    }
+    return (
+        <RNModal
+            visible={visible}
+            transparent={true}
+            onRequestClose={() => setVisible(false)}
+        >
+            <View style={styles.modalOverlay}>
+                <View style={styles.innerContainer}>
+                    <Text style={{ fontWeight: '100', color: '#888', fontStyle: 'italic' }}>Select an item...</Text>
+                    <Menu.Item
+                        // leadingIcon="redo"
+                        onPress={() => {
+                            onTitleChange('Latest repositories')
+                        }}
+                        title="Latest repositories" />
+                    <Menu.Item
+                        // leadingIcon="undo"
+                        onPress={() => { onTitleChange('Highest rated repositories') }}
+                        title="Highest rated repositories" />
+                    <Menu.Item
+                        // leadingIcon="content-cut"
+                        onPress={() => { onTitleChange('Lowest rated repositories') }}
+                        title="Lowest rated repositories" />
+                </View>
+            </View>
+        </RNModal>)
+};
+
+export default MyComponent;
--- src/RepositoryList.jsx
+++ src/RepositoryList.jsx
@@ -1,4 +1,4 @@
-import { FlatList, View, StyleSheet } from 'react-native';
+import { FlatList, View, StyleSheet, Pressable } from 'react-native';
 import RepositoryItem from './RepositoryItem';
 import { useEffect, useState } from 'react';
 import Text from './Text';
@@ -6,21 +6,41 @@ import theme from '../theme';
 import useRepositories from './hooks/useRepositories';
 import RepositoryRenderItem from './RepositoryRenderItem';
 import { ItemSeparator } from './ItemSeparator';
+import MyComponent from './Picker/Picker';
+import { List } from 'react-native-paper';
 
 const RepositoryList = () => {
     const { repositories, loading } = useRepositories();
-
     const repositoryNodes = repositories?.edges?.map(edge => edge.node) || [];
+    const [visible, setVisible] = useState(false);
+    const toggleVisibility = () => {
+        setVisible(!visible)
+        console.log(visible);
+    };
+    const [title, setTitle] = useState('Latest repositories');
+    return (
+        <>
+            <MyComponent visible={visible} setVisible={setVisible} setTitle={setTitle} />
+            {loading ? <Text style={{ fontSize: 30 }}> Loading... </Text> : <FlatList
+                data={repositoryNodes}
+                ItemSeparatorComponent={ItemSeparator}
+                aria-modal={true}
+                renderItem={({ item }) =>
+                    <RepositoryRenderItem item={item} navigatable={true} />
+                }
+                ListHeaderComponent={
+                    <>
+                        <Pressable onPress={toggleVisibility}>
+                            <List.Accordion
+                                title={title}
+                                style={{ backgroundColor: '#d3d3d3' }}
+                                onPress={toggleVisibility} />
+                        </Pressable>
+                    </>
+                }
+            />}
+        </>
 
-    return (<>
-        {loading ? <Text style={{ fontSize: 30 }}> Loading... </Text> : <FlatList
-            data={repositoryNodes}
-            ItemSeparatorComponent={ItemSeparator}
-            renderItem={({ item }) =>
-                <RepositoryRenderItem item={item} navigatable={true} />
-            }
-        />
-        }</>
     );
 };
 
